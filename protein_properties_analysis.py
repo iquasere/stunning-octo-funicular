677c07fbb685cae20692764dd3b4669482b046ed
João Sequeira <maildosequeira@gmail.com>
2016-01-27 20:38:11 +0000

    Update and rename Protein properties analysis tools to protein_properties_analysis.py

--- protein_properties_analysis.py
+++ protein_properties_analysis.py
@@ -1,26 +1,44 @@
-#Saves swissprot register  
-def select_swissprot_entries(gb_fs,ficheiro):                               #file is table retrieved from uniprot
-    handle = open(ficheiro)
+def open_swissprot():
+    handle = open("uniprot-proteome.txt")
     swiss_record = [record for record in SwissProt.parse(handle)]
+    return swiss_record
+    
+#Saves swissprot register  
+def select_swissprot_entries():
     entries = list()
-    for ft in gb_fs:
+    for ft in features:
+        encontrada = False
         if ft.type == 'CDS':
-            for entry in swiss_record:
+            for entry in open_swissprot():
                 for ref in entry.cross_references:
                     if ref[0] == 'RefSeq':
-                        if 'protein_id' in ft.qualifiers.keys():
+                        if 'protein_id' in ft.qualifiers.keys():           #three entries lack protein_id
                             if ref[1] == ft.qualifiers['protein_id'][0]:
                                 entries.append(entry)
     return entries
-    
+
 #Selects reviewed entries
-def swissprot_reviewed(sp_entries):                                         #sp_entries is result from previous function
+def swissprot_reviewed():
     r_entries = list()
-    for entry in sp_entries:
+    for entry in sse:
         if entry.data_class == 'Reviewed':
             r_entries.append(entry)
     return r_entries
-    
+
+#Counts number of entries in genbank not found in swissprot
+def check_features_in_swiss_prot():
+    not_found = 0
+    for ft in features:
+        if ft.type=='CDS':
+            found = False
+            for ss in sse:
+                if 'protein_id' in ft.qualifiers.keys():
+                    found = True
+            if found == False:
+                not_found +=1
+    return not_found
+
+
 #Search in pdb database entries corresponding to study's proteins
 def pdb():
     pdb_ids = list()
@@ -36,7 +54,7 @@ def pdb():
                             pdb_ids.append((access,pdb_id))
     f.close()
     return pdb_ids
-
+    
 #Retrieve information from pdb
 def create_pdb_files():
     pdbl = PDB.PDBList()
